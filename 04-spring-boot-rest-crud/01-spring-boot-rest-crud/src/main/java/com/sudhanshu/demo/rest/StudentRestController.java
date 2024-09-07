98bea39bb64e5e864e99d02fe22456a53256a9f2
sudhanshu-mykaarma <sudhanshu.tripathi@mykaarma.com>
2024-09-08 01:30:27 +0530

    hey there, its a spring boot app

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/sudhanshu/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/sudhanshu/demo/rest/StudentRestController.java
@@ -2,8 +2,6 @@ package com.sudhanshu.demo.rest;
 
 import com.sudhanshu.demo.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
